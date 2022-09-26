fff8f6ac924a92cf21e4f93565187b6788edb328
aluno <vagnercardozo2002@gmail.com>
2022-09-26 08:17:54 -0300

    feat: Adicionado tutorial rest 04

---
+++ posts/permissions.py
@@ -0,0 +1,13 @@
+# posts/permissions.py
+from rest_framework import permissions
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Read-only permissions are allowed for any request
+        # Permissões somente leitura são permitidas
+        # para qualquer requisição
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the author of a post
+        # As permissões de escrita são permitidas apenas
+        # ao autor de uma postagem
+        return obj.author == request.user
